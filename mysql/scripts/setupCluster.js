37731e1f35e714aca7efd4dfe30995d6be114494
Ubuntu <ubuntu@ch1-master-1.cluster.local>
2025-12-09 15:56:38 +0000

    Useless files removed, Cluster works with one or two failure but it doesn't work after down and up (RQ:rejoing)

--- mysql/scripts/setupCluster.js
+++ mysql/scripts/setupCluster.js
@@ -1,8 +1,17 @@
+
 var dbPass = "mysql"
 var clusterName = "devCluster"
 
 try {
-  print('Setting up InnoDB cluster...\n');
+
+
+  println("Starting cluster setup...");
+
+  print('Setting up InnoDB cluster... \n');
+  dba.configureInstance({user: "root", host: "mysql-server-1", password: "mysql"})
+  dba.configureInstance({user: "root", host: "mysql-server-2", password: "mysql"})
+  dba.configureInstance({user: "root", host: "mysql-server-3", password: "mysql"})
+  
   shell.connect('root@mysql-server-1:3306', dbPass)
   var cluster = dba.createCluster(clusterName);
   print('Adding instances to the cluster.');
@@ -12,5 +21,6 @@ try {
   print('.\nInstances successfully added to the cluster.');
   print('\nInnoDB cluster deployed successfully.\n');
 } catch(e) {
+  
   print('\nThe InnoDB cluster could not be created.\n\nError: ' + e.message + '\n');
 }
